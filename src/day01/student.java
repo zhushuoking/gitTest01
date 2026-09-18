1ab59b97a42f3cd1d48a586af585db2e1634c29e
zhushuo <2829293258@qq.com>
2026-09-18 21:48:06 +0800

    新增student类

---
+++ src/day01/student.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class student {
+    int age;
+    String name;
+
+    public student() {
+    }
+
+    public student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
